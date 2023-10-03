20f64e49e4dcc8287f8ae76b89864e36d456b609
alirexa.h2 <alirexa.h2@gmail.com>
2023-10-03 16:42:41 +0200

    added storage config file

--- files/get_disk.sh
+++ files/get_disk.sh
@@ -6,17 +6,18 @@ log_file="/var/log/disk_mounting.log"
 
 serial=$(cat /sys/block/$device/serial)
 
-# Read workspace data from JSON file
-workspace_file="/etc/rsc/workspace.json"
+# Read storage data from JSON file
+workspace_file="/etc/rsc/storage.json"
 workspace_data=$(cat "$workspace_file")
 
-# Parse workspace data to get required values
+# Parse storage data to get required values
 co_token=$(echo "$workspace_data" | jq -r '.co_token')
 workspace_id=$(echo "$workspace_data" | jq -r '.workspace_id')
+storage_api_endpoint=$(echo "$workspace_data" | jq -r '.storage_api_endpoint')
 
 # Execute the curl request and retrieve storages data
 ## URL should be read from workspace.json, hard coded for testing.
-curl "https://gw.rsc-pilot2.surfresearchcloud.nl/workspace/workspaces/$workspace_id/" \
+curl "$storage_api_endpoint/$workspace_id/" \
   -H "authorization: $co_token" -H "content-type: application/json" |
   jq -c ".meta.storages" | jq -c '.[]' | while IFS= read -r storage; do
   key=$(echo "$storage" | jq -r '.name' | tr ' ' '_')
