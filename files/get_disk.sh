834e4c955d247e6da12996de5ab6058bf49210a1
alirexa.h2 <alirexa.h2@gmail.com>
2023-10-16 11:34:47 +0200

    updated disk mount

--- files/get_disk.sh
+++ files/get_disk.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 device=$1
 device_path=/dev/$1
-
 log_file="/var/log/disk_mounting.log"
-
+sorage_config="/etc/rsc/storage.json"
 serial=$(cat /sys/block/$device/serial)
 
+# Check if the storage configuration file exists
+if [ ! -f "$storage_config" ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Storage configuration file not found at $storage_config"
+    exit 1
+fi
+
 # Read storage data from JSON file
-workspace_file="/etc/rsc/storage.json"
-workspace_data=$(cat "$workspace_file")
+workspace_data=$(cat "$sorage_config")
 
 # Parse storage data to get required values
 co_token=$(echo "$workspace_data" | jq -r '.co_token')
@@ -16,17 +20,42 @@ workspace_id=$(echo "$workspace_data" | jq -r '.workspace_id')
 storage_api_endpoint=$(echo "$workspace_data" | jq -r '.storage_api_endpoint')
 
 # Execute the curl request and retrieve storages data
-## URL should be read from workspace.json, hard coded for testing.
-curl "$storage_api_endpoint/$workspace_id/" \
-  -H "authorization: $co_token" -H "content-type: application/json" |
-  jq -c ".meta.storages" | jq -c '.[]' | while IFS= read -r storage; do
-  key=$(echo "$storage" | jq -r '.name' | tr ' ' '_')
-  value=$(echo "$storage" | jq -r '.volume_id' | cut -c 1-20)
-  echo "$key $value" >> "$log_file" 2>&1
+retry_count=0
+max_retries=10
+retry_delay=5
+
+while [ $retry_count -lt $max_retries ]; do
+    curl_output=$(curl "$storage_api_endpoint/$workspace_id/" -H "authorization: $co_token" -H "content-type: application/json")
+
+    # Check if the response contains key and value
+    if jq -e '.meta.storages | length > 0' <<< "$curl_output" >/dev/null; then
+        jq -c '.meta.storages | .[]' <<< "$curl_output" | while IFS= read -r storage; do
+            key=$(echo "$storage" | jq -r '.name' | tr ' ' '_')
+            value=$(echo "$storage" | jq -r '.volume_id' | cut -c 1-20)
+            echo "$key $value" >> "$log_file" 2>&1
+        done
+        break  # Exit the loop if key and value were found
+    else
+        echo "Retry $((retry_count + 1)): Volume data was not found in the endpoint. Retrying in $retry_delay seconds..."
+        sleep $retry_delay
+        retry_count=$((retry_count + 1))
+    fi
+done
+
+if [ $retry_count -eq $max_retries ]; then
+    echo "Maximum retries exceeded. Key and value not found."
+fi
+
+# curl "$storage_api_endpoint/$workspace_id/" \
+#   -H "authorization: $co_token" -H "content-type: application/json" |
+#   jq -c ".meta.storages" | jq -c '.[]' | while IFS= read -r storage; do
+#   key=$(echo "$storage" | jq -r '.name' | tr ' ' '_')
+#   value=$(echo "$storage" | jq -r '.volume_id' | cut -c 1-20)
+#   echo "$key $value" >> "$log_file" 2>&1
 
 if [ "$serial" = "$value" ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Disk ID matches: $key $device_path $serial" >> "$log_file" 2>&1
-    /opt/mount_disks.sh "$key" "$device_path" >> "$log_file" 2>&1
+    /opt/format_disks.sh "$key" "$device_path" >> "$log_file" 2>&1
   # else
   #   echo "Serial number does not match: $serial" >> "$log_file" 2>&1
   fi
--- files/mount_disks.sh
+++ files/mount_disks.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
+exec &>> /var/log/disk_mounting.log
 
-# Ensure that the script is called with the correct number of arguments
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 directory_name device_path"
-    exit 1
-fi
-
-# Extract arguments
-directory_name="$1"
-device_path="$2"
-partition="$2"1
-
-# Directory path under /data
-data_directory="/data/$directory_name"
-
-# Create the directory if it doesn't exist
-if [ ! -d "$data_directory" ]; then
-    mkdir -p "$data_directory"
-fi
+partition=/dev/$1
+data_directory=/data/$2
 
 # Check if the device is already mounted
 if mountpoint -q "$data_directory"; then
@@ -25,58 +10,41 @@ if mountpoint -q "$data_directory"; then
     exit 1
 fi
 
-# Check if the entry is already present in /etc/fstab using UUID
+if [ ! -d "$data_directory" ]; then
+    #mkdir -p "$data_directory"
+    mkdir -m 000 -p "$data_directory"
+    #Turning off all bits in the mode is a simple indicator that nobody should be allowed to do anything in this directory until a 
+    #new file system is mounted here. It's a message that this directory has been created as a mount point.
+    #It is not required for #functionality, but sometimes avoids the mistakes of creating files when the desired volume is not mounted
+fi
+
 if grep -qF "$data_directory" /etc/fstab; then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Entry already exists in /etc/fstab"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Entry $data_directory already exists in /etc/fstab"
 else
-    # Check if the device has a partition table using sfdisk
-    if sfdisk -d "$device_path" >/dev/null 2>&1; then
-        echo "$(date '+%Y-%m-%d %H:%M:%S') - Disk $device_path has partitions"
-    else
-        if [ -n "$(blkid -o value -s TYPE "$device_path")" ]; then
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Existing filesystem found on $device_path"
-        else
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Disk $device_path is raw and safe to format"
-            # Format the disk with xfs
-            sgdisk -o $device_path
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - made gpt label for $device_path"
-
-            sgdisk --new 1::0 $device_path
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - partition "$device_path"1 created"
-
-            mkfs.xfs "$device_path"1
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - ${device_path}1 formatted as XFS"
-
-            xfs_admin -L "$directory_name" "$device_path"1
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - XFS label added"
-            
-            sleep 2;
-            partprobe "$device_path"
-        fi
-    fi
-    
-    # Check if the mount operation was successful
     if [ -n "$partition" ] && [ -n "$data_directory" ]; then
         echo "$(date '+%Y-%m-%d %H:%M:%S') - Mounting $partition on $data_directory"
-        #mount "$partition" "$data_directory"
-        /usr/bin/systemd-mount -G "$partition" "$data_directory"
-        
-        # Check if the mount operation was successful
-        if [ $? -eq 0 ]; then
-            # Add an entry to /etc/fstab using the partition's UUID
-            partition_uuid=$(blkid -s UUID -o value "$partition")
-            
-            if [ -n "$partition_uuid" ]; then
-                echo "UUID=$partition_uuid $data_directory xfs defaults 0 0" >> /etc/fstab
-                echo "$(date '+%Y-%m-%d %H:%M:%S') - Mounting complete for directory: $data_directory"
+        /usr/bin/systemd-mount --fsck=no --no-block -G "$partition" "$data_directory" >> /var/log/disk_mounting.log 2>&1
+
+            # Check if the mount operation was successful
+            if [ $? -eq 0 ]; then
+                # Add an entry to /etc/fstab using the partition's UUID
+                partition_uuid=$(blkid -s UUID -o value "$partition")
+                
+                if [ -n "$partition_uuid" ]; then
+                    # to enable growfs, add x-systemd.growfs to fstab line and do not create a partition on the disk during formatting.
+                    echo "UUID=$partition_uuid $data_directory xfs defaults,x-systemd.device-timeout=9s,x-systemd.mount 0 0" >> /etc/fstab
+                    #echo "UUID=$partition_uuid $data_directory xfs defaults,noauto,x-systemd.device-timeout=9s,x-systemd.automount 0 0" >> /etc/fstab
+                    echo "$(date '+%Y-%m-%d %H:%M:%S') - Mounting complete for directory: $data_directory"
+                else
+                    echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Failed to retrieve partition UUID."
+                fi
             else
-                echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Failed to retrieve partition UUID."
+                systemctl start $data_directory
+                if [ $? -eq 0 ]; then
+                    echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Mounting failed for partition: $partition"
+                fi
             fi
         else
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Mounting failed for partition: $partition"
-        fi
-    else
-        echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Both partition and data_directory must be provided and non-empty."
+            echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Both partition and data_directory must be provided and non-empty."
     fi
-
 fi
