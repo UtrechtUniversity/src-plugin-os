fcac0b9cf38f6c956f4f31b6c0d3281329aa2d62
alirexa.h2 <alirexa.h2@gmail.com>
2023-11-28 06:15:42 +0100

    removed comments

--- files/format_rsc_disk.sh
+++ files/format_rsc_disk.sh
@@ -10,9 +10,7 @@ fi
 device="$1"
 device_path="/dev/$1"
 directory_name="$(/opt/rsc-utilities/get_disk.sh $device)"
-#partition="$2"1
 data_directory="/data/$directory_name"
-
 
 # Check if the entry is already present in /etc/fstab using UUID
 if grep -qF "$data_directory" /etc/fstab; then
@@ -41,34 +39,6 @@ else
 
             xfs_admin -L "$directory_name" "$device_path"1
             echo "$(date '+%Y-%m-%d %H:%M:%S') - XFS label added for device $device_path"
-            # partprobe "$device_path"
         fi
     fi
-    
-    # if [ -n "$partition" ] && [ -n "$data_directory" ]; then
-    #     echo "$(date '+%Y-%m-%d %H:%M:%S') - Mounting $partition on $data_directory"
-    #     #mount "$partition" "$data_directory"
-        
-    #     /usr/bin/systemd-mount --fsck=no --no-block -G "$partition" "$data_directory"
-        
-#         # Check if the mount operation was successful
-#         if [ $? -eq 0 ]; then
-#             # Add an entry to /etc/fstab using the partition's UUID
-#             partition_uuid=$(blkid -s UUID -o value "$partition")
-            
-#             if [ -n "$partition_uuid" ]; then
-#                 # to enable growfs, add x-systemd.growfs to fstab line and do not create a partition on the disk during formatting.
-#                 echo "UUID=$partition_uuid $data_directory xfs defaults,noauto,x-systemd.device-timeout=9s,x-systemd.mount 0 0" >> /etc/fstab
-#                 #echo "UUID=$partition_uuid $data_directory xfs defaults,noauto,x-systemd.device-timeout=9s,x-systemd.automount 0 0" >> /etc/fstab
-#                 echo "$(date '+%Y-%m-%d %H:%M:%S') - Mounting complete for directory: $data_directory"
-#             else
-#                 echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Failed to retrieve partition UUID."
-#             fi
-#         else
-#             echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Mounting failed for partition: $partition"
-#         fi
-#     else
-#         echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Both partition and data_directory must be provided and non-empty."
-#     fi
-
 fi
