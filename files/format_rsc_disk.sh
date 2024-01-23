540b29e8d3793304f5d5139c6d5174f05146a83c
alirexa.h2 <alirexa.h2@gmail.com>
2024-01-23 17:31:38 +0100

    install gdisk on centos 7

--- files/format_rsc_disk.sh
+++ files/format_rsc_disk.sh
@@ -11,25 +11,29 @@ device_path=$1
 directory_name=$2
 
 # Check if the device has a partition table using sfdisk
-if  blkid -o value -s TYPE "$device_path" &> /dev/null; then
+if parted -s "$device_path" print | grep -q "Partition Table: \(gpt\|msdos\)"; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Existing filesystem found on $device_path"
 else
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Disk $device_path is raw and safe to format"
-    # Format the disk with xfs
-    sgdisk -o $device_path
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - made gpt label for $device_path"
+    if  blkid -o value -s TYPE "$device_path" &> /dev/null; then
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Existing filesystem found on $device_path"
+    else
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Disk $device_path is raw and safe to format"
+        # Format the disk with xfs
+        sgdisk -o $device_path
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - made gpt label for $device_path"
+        
+        sgdisk --new 1::0 $device_path
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - partition "$device_path"1 created"
+        #Setting GPT PARTLABEL
+        sgdisk -c 1:"$directory_name" "$device_path"
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - GPT Partition lable $directory_name created for device $device_path"
 
-    sgdisk --new 1::0 $device_path
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - partition "$device_path"1 created"
-    #Setting GPT PARTLABEL
-    sgdisk -c 1:"$directory_name" "$device_path"
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - GPT Partition lable $directory_name created for device $device_path"
+        mkfs.xfs "$device_path"1
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - ${device_path}1 formatted as XFS"
 
-    mkfs.xfs "$device_path"1
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - ${device_path}1 formatted as XFS"
-
-    xfs_admin -L "$directory_name" "$device_path"1
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - XFS label added for device $device_path"
+        xfs_admin -L "$directory_name" "$device_path"1
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - XFS label added for device $device_path"
+    fi
 fi
 
 sleep 1
